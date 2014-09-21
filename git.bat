676122a482b46da05c7715ddd3e13f67ce9c4b07
Daniel Nichols <thebagmaster@gmail.com>
2014-09-20 22:13:11 -0500

    Added Service

--- git.bat
+++ git.bat
@@ -1,8 +1,5 @@
-git.exe init
 git.exe add .
-git.exe commit -m "First Commit"
-git.exe remote add origin https://github.com/thebagmaster/WEB_Craigslist_Crawler.git
-git.exe pull https://github.com/thebagmaster/WEB_Craigslist_Crawler.git
+git.exe commit -m "Added Service"
 git.exe push origin master
 echo Press Enter...
 read
